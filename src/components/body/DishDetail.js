4ac157be07151071c2af9dcdc87b21ba6a6bd0e7
Suresh Chakma <suresh.chakma.cht@gmail.com>
2021-07-24 21:01:40 +0600

    Restaurant project

--- src/components/body/DishDetail.js
+++ src/components/body/DishDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText} from 'reactstrap';
+import LoadComments from './LoadComments';
 
 const DishDetail = props => {
     return(
@@ -8,10 +9,10 @@ const DishDetail = props => {
                 <CardImg top src ={props.dish.image} alt ={props.dish.name} />
                 <CardBody style ={{textAlign: "left"}} >
                     <CardTitle>{props.dish.name}</CardTitle>
-                    <CardText>
-                    <p>{props.dish.description}</p> 
-                    <p>Price: ${props.dish.price} /-</p>
-                    </CardText>
+                    <CardText>{props.dish.description}</CardText>
+                    <CardText>Price: ${props.dish.price} /-</CardText>
+                    <hr />
+                    <LoadComments comments ={props.dish.comments} />
                 </CardBody>
             </Card>
         </div>
